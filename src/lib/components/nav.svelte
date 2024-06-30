4f41c752c97aaed4f0dab0e4fc4ef5a5b1a0f129
mwenya <mwenyamail877@gmail.com>
2024-06-30 20:05:37 +0200

    This should fix it

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,13 +1,20 @@
-
+<script>
+   import { Link } from "svelte-routing";
+</script>
 <nav class="flex items-center justify-between flex-wrap p-6">
     <div class="flex items-center flex-shrink-0 text-white mr-6 font-bold">
      Mwenya Chibwe
     </div>
       <div>
-        <a href="/" class="inline-block text-sm px-4 py-2 leading-none  text-whitelg:mt-0">Home</a>
-        <a href="/projects" class="inline-block text-sm px-4 py-2 leading-none  text-whitelg:mt-0">Projects</a>
-        <a href="/contacts" class="inline-block text-sm px-4 py-2 leading-none  text-whitelg:mt-0">Contact Me</a>
-     
+        <Link to="/">
+          <p class="inline-block text-sm px-4 py-2 leading-none  text-whitelg:mt-0">Home</p>
+        </Link>
+        <Link to="/projects">
+          <p class="inline-block text-sm px-4 py-2 leading-none  text-whitelg:mt-0">Projects</p>
+        </Link>
+        <Link to="/contacts">
+          <p class="inline-block text-sm px-4 py-2 leading-none  text-whitelg:mt-0">Contact Me</p>
+        </Link>
       </div>
   </nav>
 
