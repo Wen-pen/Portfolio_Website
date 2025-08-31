2aabded19f18eaabfb14600362886ddc3a4a517c
mwenya <mwenyamail877@gmail.com>
2025-08-31 21:45:35 +0100

    Made changes to no longer need GSAP, also updated text for second year

--- src/lib/pages/home.svelte
+++ src/lib/pages/home.svelte
@@ -1,41 +1,35 @@
 <script>
     import Button from "../components/button.svelte"
-    import { gsap } from "gsap/dist/gsap"
+    import { fade } from 'svelte/transition'
     import { onMount } from 'svelte'
-    import { TextPlugin } from "gsap/dist/TextPlugin"
-  import { Link } from "svelte-routing";
+    import { Link } from "svelte-routing";
 
-    let main
-    gsap.registerPlugin(TextPlugin);
-    onMount(() =>{
-        if (main){
-            gsap.from("#main" ,{
-            duration: 5, 
-            opacity: 0,
-        })
-        }
-        })
+  let visible = false
+  onMount(() => {
+    requestAnimationFrame(()=>{
+        setTimeout(()=>{
+            visible = true
+        }, 0)
+    })})
+   
 </script>
- 
-<main bind:this={main} class="min-h-screen bg-gradient-to-t from-[#DEE4EA] to-[#7D8184] text-white bg-blend-multiply font-poppins">
+ {#if visible}
+<main  class="min-h-screen bg-gradient-to-t from-[#DEE4EA] to-[#7D8184] text-white bg-blend-multiply font-poppins">
     <div class="flex items-center justify-center h-32">
         <div class="m-6 w-32 bg-grey-400"></div>
     </div>
-        
-    <h1 class="text-6xl text-center" id="main" >
-       
+    <div transition:fade={{delay: 0, duration: 8000}} class="anim-wrap">
+     <h1 class="text-6xl text-center" id="main" > 
         🧑🏾‍💻
-    </h1>
-    
-    <h1 class="text-white text-center font-extrabold m-3 text-4xl" id="main">
-        
+    </h1> 
+    <h1 class="text-white text-center font-extrabold m-3 text-4xl" id="main">  
         Mwenya Chibwe
     </h1>
 
         <div class="flex flex-col items-center" id="main">
             <div class="w-96">
                 <p class="text-white m-3 text-center shrink font-normal">
-                    A 19 year old first year computer science student at Nottingham Trent University. Experienced in Python, JS, C# and Dart. 
+                    A 20 year old second year computer science student at Nottingham Trent University. Committee Member of the Developers Society.
                 </p>
                 <p class="text-white m-3 text-center shrink font-normal">
                     While my current hobbies are debating, video gaming and weightlifting, programming is my first love 🤍
@@ -54,5 +48,8 @@
                  </Link>
         
         </div>
+    </div>
+   
 </main>
+{/if}
 
--- src/lib/pages/projects.svelte
+++ src/lib/pages/projects.svelte
@@ -1,10 +1,18 @@
 <script>
-    import { gsap } from "gsap/dist/gsap"
     import { onMount } from 'svelte'
-    import { TextPlugin } from "gsap/dist/TextPlugin"
+     import { fade } from 'svelte/transition'
     import Nav from "../components/nav.svelte"
     import Card from "../components/card.svelte";
     let main  
+
+
+  let visible = false
+  onMount(() => {
+    requestAnimationFrame(()=>{
+        setTimeout(()=>{
+            visible = true
+        }, 0)
+    })})
 
     let globalRepoArr = [];
 
@@ -32,14 +40,20 @@ github_grabber().then(() => {
 });
 </script>
 
+{#if visible}
 <main bind:this={main} class="min-h-screen bg-gradient-to-t from-[#DEE4EA] to-[#7D8184] text-white bg-blend-multiply font-poppins  text-wrap">
     <Nav/>
+    
     <h1 class="text-center font-bold text-4xl p-3" id="main">
         My Projects
     </h1>
+    <div class="anim-wrap" transition:fade={{delay: 0, duration: 8000}}>
     <div class=" md:grid grid-cols-2 text-wrap" id="main">
        {#each globalRepoArr as repo}
             <Card title={repo.name} description={repo.description} link={repo.link}/>
        {/each}
     </div> 
+    </div>
+   
 </main>
+{/if}
